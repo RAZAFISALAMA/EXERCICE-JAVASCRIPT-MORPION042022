08ab07885d5f6a4ceac61e9a4d61a79a1b47dab4
RAZAFISALAMA <98509559+RAZAFISALAMA@users.noreply.github.com>
2022-05-09 16:33:12 +0300

    Add files via upload

---
+++ devoir javasr/index.js
@@ -0,0 +1,27 @@
+ vargrid="iteam1","item2", "iteam3","iteam4","itaem5","iteam6","iteam7", "aiteam8", "iteam9";
+ var grid1=[];
+ var grid2=[];
+ var grid3=[];
+ var grid4=[];
+ var grid5=[];
+ var grid6=[];
+ var grid7=[];
+ var grid8=[];
+ var grid9=[];
+ var succes; 
+ var choise=0;
+ var gamOver=0;
+ varplayer=0;
+ none="player1";
+ team;
+
+  over=[]; var espace={
+      non="conmputer"
+      team="",over
+  };
+  var player=1{
+      none="player1"
+var=[]
+
+      }
+  
